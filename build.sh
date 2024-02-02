ae39e8f320af1672017ed80850782e1bfcaa12ea
MrErenK <akbaseren4751@gmail.com>
2024-02-02 17:10:47 +0000

    Fixes

--- build.sh
+++ build.sh
@@ -2,8 +2,19 @@
 
 source config.env
 
+# Create the build directory
+
+if [ -f rom/.repo/manifest.xml ]; then
+    echo "Repo already initialized"
+    cd rom
+    exit 1
+else
+    mkdir -p rom
+    cd rom
+fi
+
 # Init the repo
-repo init -u ${repo} -b ${branch}
+repo init -u ${repo} -b ${repo_branch}
 
 # Sync the repo
 repo sync ${sync_flags}
