009374235885ace7cc5fb65ffbfb028fc0e549e9
MrErenK <akbaseren4751@gmail.com>
2024-02-10 18:12:47 +0000

    Rewrite code

--- build.sh
+++ build.sh
@@ -1,188 +1,21 @@
 #!/usr/bin/env bash
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Source configuration variables
 source config.env
-main_dir="$(readlink -f -- $(pwd))/rom"
 
 # Set git config
 git config --global user.email "${git_email}"
 git config --global user.name "${git_name}"
 
-# Create the build directory
-if [ -f rom/.repo/manifest.xml ]; then
-    echo "[!] Repo already initialized"
-    cd ${main_dir}
-else
-    mkdir -p ${main_dir}
-    cd ${main_dir}
-    # Init the repo
-    echo "[*] Initializing the repo"
-    repo init -u ${repo} -b ${repo_branch} --depth=10
-fi
+# Define main directory
+main_dir="$(readlink -f -- $(pwd))/rom"
+cd "${main_dir}"
 
-# Sync the repo
-if [ -d .repo ]; then
-    if [ "${should_skip_sync}" = "1" ]; then
-        echo "[!] Skipping sync"
-    else
-        echo "[*] Syncing the repo"
-        repo sync ${sync_args}
-        apply_patches
-    fi
-else
-    echo "[!] Repo not initialized"
-    exit 1
-fi
-
-# Clone the device tree
-if [ -d "${device_tree_path}" ]; then
-    echo "[!] Device tree already cloned"
-else
-    echo "[*] Cloning the device tree"
-    git clone ${device_tree_clone}
-fi
-
-# Clone the sepolicy tree
-if [ -d "${sepolicy_tree_path}" ]; then
-    echo "[!] Sepolicy already cloned"
-else
-    echo "[*] Cloning the sepolicy tree"
-    git clone ${sepolicy_tree_clone}
-fi
-
-# Clone the vendor tree
-if [ -d "${vendor_tree_path}" ]; then
-    echo "[!] Vendor tree already cloned"
-else
-    echo "[*] Cloning the vendor tree"
-    git clone ${vendor_tree_clone}
-fi
-
-# Clone the ims vendor tree
-if [ -d "${ims_vendor_tree_path}" ]; then
-    echo "[!] IMS Vendor tree already cloned"
-else
-    echo "[*] Cloning the IMS vendor tree"
-    git clone ${ims_vendor_tree_clone}
-fi
-
-# Clone the firmware vendor tree
-if [ -d "${fw_vendor_tree_path}" ]; then
-    echo "[!] Firmware Vendor tree already cloned"
-else
-    echo "[*] Cloning the Firmware vendor tree"
-    git clone ${fw_vendor_tree_clone}
-fi
-
-# Clone the kernel
-if [ -d "${kernel_tree_path}" ]; then
-    echo "[!] Kernel already cloned"
-else
-    echo "[*] Cloning the kernel"
-    git clone ${kernel_tree_clone}
-fi
-
-# Clone and update the extra repos
-if [ -n "${extra_repos_clone}" ]; then
-   IFS='|' read -r -a extra_repos_clone <<< "${extra_repos_clone}"
-   IFS='|' read -r -a extra_repos_path <<< "${extra_repos_path}"
-   IFS='|' read -r -a extra_repos_branch <<< "${extra_repos_branch}"
-   for index in "${!extra_repos_clone[@]}"
-   do
-       echo "[*] Updating extra repo ${extra_repos_path[index]}"
-       repo_path="${main_dir}/${extra_repos_path[index]}"
-       if [ ! -d "$repo_path" ]; then
-            # Clone the repo if it does not exist
-            echo "[*] Cloning extra repo ${extra_repos_path[index]}"
-            git clone -b "${extra_repos_branch[index]}" "${extra_repos_clone[index]}" "$repo_path"
-       fi
-       pushd . # Save current directory
-       cd "$repo_path"
-       git fetch origin "${extra_repos_branch[index]}" && git pull origin "${extra_repos_branch[index]}" || echo "[!] Failed to update ${extra_repos_path[index]}"
-       popd # Return to the saved directory
-   done
-else
-    echo "[!] No extra repos to clone"
-fi
-
-# Apply patches function
-apply_patches()
-{
-    if [ -f "../patches.sh" ]; then
-        echo "[*] Applying patches"
-        cp ../patches.sh .
-        chmod +x patches.sh
-        ./patches.sh
-        rm patches.sh
-    else
-        echo "[!] No patches to apply"
-    fi
-}
-
-# Update the device repos
-if [ "${should_update_trees}" = "1" ]; then
-    echo "[*] Updating device repos"
-    cd ${device_tree_path}
-    git fetch origin ${device_tree_branch} && git pull origin ${device_tree_branch} || echo "[!] Failed to update device repos"
-    cd ${main_dir}
-fi
-
-# Update the sepolicy repos
-if [ "${should_update_trees}" = "1" ]; then
-    echo "[*] Updating sepolicy repos"
-    cd ${sepolicy_tree_path}
-    git fetch origin ${sepolicy_tree_branch} && git pull origin ${sepolicy_tree_branch} || echo "[!] Failed to update sepolicy repos"
-    cd ${main_dir}
-fi
-
-# Update the vendor repos
-if [ "${should_update_trees}" = "1" ]; then
-    echo "[*] Updating vendor repos"
-    cd ${vendor_tree_path}
-    git fetch origin ${vendor_tree_branch} && git pull origin ${vendor_tree_branch} || echo "[!] Failed to update vendor repos"
-    cd ${main_dir}
-fi
-
-# Update the ims vendor repos
-if [ "${should_update_trees}" = "1" ]; then
-    echo "[*] Updating IMS vendor repos"
-    cd ${ims_vendor_tree_path}
-    git fetch origin ${ims_vendor_tree_branch} && git pull origin ${ims_vendor_tree_branch} || echo "[!] Failed to update IMS vendor repos"
-    cd ${main_dir}
-fi
-
-# Update the firmware vendor repos
-if [ "${should_update_trees}" = "1" ]; then
-    echo "[*] Updating Firmware vendor repos"
-    cd ${fw_vendor_tree_path}
-    git fetch origin ${fw_vendor_tree_branch} && git pull origin ${fw_vendor_tree_branch} || echo "[!] Failed to update Firmware vendor repos"
-    cd ${main_dir}
-fi
-
-# Update the kernel repos
-if [ "${should_update_kernel}" = "1" ]; then
-    echo "[*] Updating kernel repos"
-    cd ${kernel_tree_path}
-    git fetch origin ${kernel_tree_branch} && git pull origin ${kernel_tree_branch} || echo "[!] Failed to update kernel repos"
-    cd ${main_dir}
-fi
-
-# Clean the out directory if needed
-if [ "${clean_out}" = "1" ]; then
-    echo "[*] Cleaning the out directory"
-    cd ${main_dir}
-    rm -rf out/
-fi
-
-# Clone and set telegram stuff
-if [ ! -f "./Telegram/telegram" ]
-then
-  git clone --depth=1 https://github.com/fabianonline/telegram.sh Telegram
-fi
-TELEGRAM=./Telegram/telegram
-
-# Function to send telegram messages
-send_msg()
-{
+# Function to send Telegram messages
+send_msg() {
   "${TELEGRAM}" -H -D \
       "$(
           for POST in "${@}"; do
@@ -190,6 +23,143 @@ send_msg()
           done
       )"
 }
+
+# Function to clone repositories
+clone_repo() {
+    local repo_path="${1}"
+    local clone_url="${2}"
+    local repo_name="${3}"
+    local branch="${4:-master}"
+    local depth="${5}"  # Depth value provided as argument, if any
+    if [ -d "${repo_path}" ]; then
+        echo "[!] ${repo_name} already cloned"
+    else
+        echo "[*] Cloning ${repo_name}"
+        if [ -n "$depth" ]; then
+            git clone --depth "${depth}" -b "${branch}" "${clone_url}" "${repo_path}"
+        else
+            git clone -b "${branch}" "${clone_url}" "${repo_path}"
+        fi
+    fi
+}
+
+# Function to update repositories
+update_repo() {
+    local repo_path="${1}"
+    local branch="${2}"
+    local repo_name="${3}"
+    if [ -d "${repo_path}" ]; then
+        echo "[*] Updating ${repo_name}"
+        cd "${repo_path}"
+        if git fetch origin "${branch}" 2>/dev/null; then
+            git pull origin "${branch}" || echo "[!] Failed to update ${repo_name}"
+        else
+            echo "[!] Branch ${branch} doesn't exist on the remote repository"
+        fi
+        cd "${main_dir}"
+    else
+        echo "[!] ${repo_name} not found"
+    fi
+}
+
+# Function to upload ROM
+upload_rom() {
+    if [ "${PD_UPLOAD}" = "true" ]; then
+        cd "${main_dir}"
+        export TARGET_DIR="${main_dir}/out/target/product/${device_codename}"
+        zip_path="$(python3 ../get_rom_zip.py)"
+        if [ -f "${zip_path}" ]; then
+            echo "[*] Uploading the ROM"
+            export FILE_ID="$(curl -sT "${zip_path}" https://pixeldrain.com/api/file/ | grep -o '"id":"[^"]*' | awk -F ':"' '{print $2}')"
+            echo "[*] Download the ROM at: https://pixeldrain.com/u/${FILE_ID}"
+        else
+            echo "[!] No ROM to upload"
+        fi
+    fi
+}
+
+# Create the build directory if not already initialized
+if [ -f ".repo/manifest.xml" ]; then
+    echo "[!] Repo already initialized"
+    cd "${main_dir}" || exit 1
+else
+    mkdir -p "${main_dir}"
+    cd "${main_dir}" || exit 1
+    # Init the repo
+    echo "[*] Initializing the repo"
+    repo init -u "${repo}" -b "${repo_branch}" --depth=10
+fi
+
+# Sync the repo if initialized
+if [ -d ".repo" ]; then
+    if [ "${should_skip_sync}" = "1" ]; then
+        echo "[!] Skipping sync"
+    else
+        echo "[*] Syncing the repo"
+        repo sync "${sync_args}"
+        apply_patches
+    fi
+else
+    echo "[!] Repo not initialized"
+    exit 1
+fi
+
+# Clone repositories
+clone_repo "${device_tree_path}" "${device_tree_clone}" "Device tree" "${device_tree_branch}"
+clone_repo "${sepolicy_tree_path}" "${sepolicy_tree_clone}" "Sepolicy tree" "${sepolicy_tree_branch}"
+clone_repo "${vendor_tree_path}" "${vendor_tree_clone}" "Vendor tree" "${vendor_tree_branch}" 15
+clone_repo "${ims_vendor_tree_path}" "${ims_vendor_tree_clone}" "IMS Vendor tree" "${ims_vendor_tree_branch}" 1
+clone_repo "${fw_vendor_tree_path}" "${fw_vendor_tree_clone}" "Firmware Vendor tree" "${fw_vendor_tree_branch}" 1
+clone_repo "${kernel_tree_path}" "${kernel_tree_clone}" "Kernel" "${kernel_tree_branch}" 250
+
+# Clone and update extra repos
+if [ -n "${extra_repos_clone}" ]; then
+    IFS='|' read -r -a extra_repos_clone <<< "${extra_repos_clone}"
+    IFS='|' read -r -a extra_repos_path <<< "${extra_repos_path}"
+    IFS='|' read -r -a extra_repos_branch <<< "${extra_repos_branch}"
+    
+    for index in "${!extra_repos_clone[@]}"; do
+        repo_path="${main_dir}/${extra_repos_path[index]}"
+        clone_repo "${repo_path}" "${extra_repos_clone[index]}" "Extra Repo ${extra_repos_path[index]}" "${extra_repos_branch[index]}"
+        update_repo "${repo_path}" "${extra_repos_branch[index]}" "Extra Repo ${extra_repos_path[index]}"
+    done
+else
+    echo "[!] No extra repos to clone"
+fi
+
+# Update repositories if needed
+if [ "${should_update_trees}" = "1" ]; then
+    update_repo "${device_tree_path}" "${device_tree_branch}" "Device repos"
+    update_repo "${sepolicy_tree_path}" "${sepolicy_tree_branch}" "Sepolicy repos"
+    update_repo "${vendor_tree_path}" "${vendor_tree_branch}" "Vendor repos"
+    update_repo "${ims_vendor_tree_path}" "${ims_vendor_tree_branch}" "IMS Vendor repos"
+    update_repo "${fw_vendor_tree_path}" "${fw_vendor_tree_branch}" "Firmware Vendor repos"
+    update_repo "${kernel_tree_path}" "${kernel_tree_branch}" "Kernel repos"
+fi
+
+# Apply patches
+if [ -f "../patches.sh" ]; then
+    echo "[*] Applying patches"
+    cp ../patches.sh .
+    chmod +x patches.sh
+    ./patches.sh
+    rm patches.sh
+else
+    echo "[!] No patches to apply"
+fi
+
+# Clean the out directory if needed
+if [ "${clean_out}" = "1" ]; then
+    echo "[*] Cleaning the out directory"
+    cd "${main_dir}"
+    rm -rf out/
+fi
+
+# Clone and set Telegram stuff
+if [ ! -f "./Telegram/telegram" ]; then
+  git clone --depth=1 https://github.com/fabianonline/telegram.sh Telegram
+fi
+TELEGRAM=./Telegram/telegram
 
 # Send a message to the Telegram channel
 if [ "${TELEGRAM_TOKEN}" != "" ] && [ "${TELEGRAM_CHAT}" != "" ]; then
@@ -200,37 +170,24 @@ if [ "${TELEGRAM_TOKEN}" != "" ] && [ "${TELEGRAM_CHAT}" != "" ]; then
     send_msg "${MSG}"
 fi
 
-# Upload ROM function
-upload_rom()
-{
-    if [ "${PD_UPLOAD}" = "true" ]; then
-        cd ${main_dir}
-        export TARGET_DIR="${main_dir}/out/target/product/${device_codename}"
-        zip_path="$(python3 ../get_rom_zip.py)"
-        if [ -f "${zip_path}" ]; then
-            echo "[*] Uploading the ROM"
-            export FILE_ID="$(curl -sT "${zip_path}" https://pixeldrain.com/api/file/ | grep -o '"id":"[^"]*' | awk -F ':"' '{print $2}')" || echo "[!] Failed to upload the ROM" && send_msg "Failed to upload the ROM"
-            echo "[*] Download the ROM at: https://pixeldrain.com/u/${FILE_ID}"
-        else
-            echo "[!] No ROM to upload"
-        fi
-    fi
-}
-
 # Build the ROM
-echo "[*] Starting the build" && echo "[*] Building ${device_codename}"
-cd ${main_dir}
+echo "[*] Starting the build"
+echo "[*] Building ${device_codename}"
+cd "${main_dir}"
 . build/envsetup.sh
 
-# Do a installclean if needed
+# Do an installclean if needed
 if [ "${installclean}" = "1" ]; then
     echo "[*] Running installclean"
     make installclean
 fi
 
-lunch ${lunch_target}
-${make_cmd} && echo "[*] Build completed successfully" && upload_rom || echo "[!] Build failed" && send_msg "Build failed for ${device_codename}
--Log: https://ci.erensprojects.me/job/${JOB_NAME}/ws/rom/out/error.log" && exit 1
+lunch "${lunch_target}"
+${make_cmd} || { echo "[!] Build failed"; send_msg "Build failed for ${device_codename} - Log: https://ci.erensprojects.me/job/${JOB_NAME}/ws/rom/out/error.log"; exit 1; }
+
+# If the build is successful, upload ROM
+echo "[*] Build completed successfully"
+upload_rom
 
 # Send a message to the Telegram channel
 if [ "${TELEGRAM_TOKEN}" != "" ] && [ "${TELEGRAM_CHAT}" != "" ]; then
@@ -248,3 +205,5 @@ if [ "${TELEGRAM_TOKEN}" != "" ] && [ "${TELEGRAM_CHAT}" != "" ]; then
         send_msg "${MSG}"
     fi
 fi
+
+exit 0
